58731366054d0ae030e13e6e8566561d58a358ac
techmsi <sophia.idro@gmail.com>
2014-07-24 15:21:31 -0400

    Added more tests

--- test/test.js
+++ test/test.js
@@ -1,16 +1,24 @@
 var should = require('chai').should();
 var nunjucksdate = require('../index');
-var date = nunjucksdate.date;
+var getFilter = nunjucksdate.getFilter;
 var setDefaultFormat = nunjucksdate.setDefaultFormat;
 
+describe('#getFilter', function() {
+  it('converts Dec 25, 1995 to 12-25-1995', function() {
+    getFilter('Dec 25, 1995', 'MM-DD-YYYY').should.equal('12-25-1995');
+  });
 
-describe('#date', function() {
+  it('converts Dec 25, 1995 to 12-25-1995 am', function() {
+    getFilter('Dec 25, 1995', 'MM-DD-YYYY a').should.equal('12-25-1995 am');
+  });
+
+  it('converts Dec 25, 1995 to 12-25-1995 a.m.', function() {
+    getFilter('Dec 25, 1995', 'MM-DD-YYYY a',true).should.equal('12-25-1995 a.m.');
+  });
+});
+
+describe('#setDefaultFormat', function() {
   it('sets default format to MMMM Do YYYY, h:mm:ss a', function() {
     setDefaultFormat('MMMM Do YYYY, h:mm:ss a').should.equal('MMMM Do YYYY, h:mm:ss a');
   });
-
-  it('converts Dec 25, 1995 to 12-25-1995', function() {
-    date('Dec 25, 1995', 'MM-DD-YYYY').should.equal('12-25-1995');
-  });
-
 });
