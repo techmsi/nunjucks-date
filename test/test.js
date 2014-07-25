9e53c52dfd7996b8f607f7e1a2262705f3d0f3a7
techmsi <sophia.idro@gmail.com>
2014-07-25 12:24:55 -0400

    Test: Modified tests to be more descriptive.

--- test/test.js
+++ test/test.js
@@ -1,24 +1,45 @@
 var should = require('chai').should();
 var nunjucksdate = require('../index');
 var getFilter = nunjucksdate;
-var setDefaultFormat = nunjucksdate.setDefaultFormat;
+var dateStringTest = 'Dec 25, 1995';
 
-describe('#getFilter', function() {
-  it('converts Dec 25, 1995 to 12-25-1995', function() {
-    getFilter('Dec 25, 1995', 'MM-DD-YYYY').should.equal('12-25-1995');
+describe('#getFilter - 1st case - specified string.\n Uses default date format.', function() {
+  it('converts  ' + dateStringTest + '  to 1995', function() {
+    getFilter(dateStringTest).should.equal('1995');
+  });
+});
+
+describe('#getFilter - 2nd case - specified string & meridiem=true.\n Uses default date format.', function() {
+  it('converts  ' + dateStringTest + '  to 1995', function() {
+    getFilter(dateStringTest, true).should.equal('1995');
+  });
+});
+
+describe('#getFilter - 3rd case - specified string, format, & meridiem=true', function() {
+  it('converts ' + dateStringTest + ' to 12-25-1995 a.m. ', function() {
+    getFilter(dateStringTest, 'MM-DD-YYYY a',true).should.equal('12-25-1995 a.m.');
+  });
+});
+
+describe('#getFilter - 4th case - specified string & format', function() {
+
+  it('converts  ' + dateStringTest + '  to 12-25-1995', function() {
+    getFilter(dateStringTest, 'MM-DD-YYYY').should.equal('12-25-1995');
   });
 
-  it('converts Dec 25, 1995 to 12-25-1995 am', function() {
-    getFilter('Dec 25, 1995', 'MM-DD-YYYY a').should.equal('12-25-1995 am');
-  });
-
-  it('converts Dec 25, 1995 to 12-25-1995 a.m.', function() {
-    getFilter('Dec 25, 1995', 'MM-DD-YYYY a',true).should.equal('12-25-1995 a.m.');
+  it('converts ' + dateStringTest + ' to 12-25-1995 am', function() {
+    getFilter(dateStringTest, 'MM-DD-YYYY a').should.equal('12-25-1995 am');
   });
 });
 
 describe('#setDefaultFormat', function() {
   it('sets default format to MMMM Do YYYY, h:mm:ss a', function() {
-    setDefaultFormat('MMMM Do YYYY, h:mm:ss a').should.equal('MMMM Do YYYY, h:mm:ss a');
+    nunjucksdate.setDefaultFormat('MMMM Do YYYY, h:mm:ss a').should.equal('MMMM Do YYYY, h:mm:ss a');
+  });
+});
+
+describe('#install', function() {
+  it('installs this plugin', function() {
+    //TODO: implement test for installing filter in nunjucks environment
   });
 });
