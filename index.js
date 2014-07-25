8e9100859bbc8e4a0d87fa3ee916397c074a3bb7
techmsi <sophia.idro@gmail.com>
2014-07-25 10:47:40 -0400

    Module: Added setCustomName functionality.

--- index.js
+++ index.js
@@ -2,21 +2,22 @@ var moment = require('moment');
 
 var meridiemRegEx = new RegExp('(a{1,2}|p)\.?m{1}?\.?', 'i');
 var defaultFormat = 'YYYY';
+var customName = null;
 
 function getFilter(dateString) {
     if (arguments.length === 1) {
-        // getFilter('March 1st')
+        // getFilter('Dec 25, 1995')
         return moment(dateString).format(defaultFormat);
     } else if (arguments.length === 2 && typeof arguments[1] === 'boolean') {
-        // getFilter('March 1st', true)
+        // getFilter('Dec 25, 1995', true)
         return moment(dateString).format(defaultFormat).replace(meridiemRegEx, "$1.m.");
     } else if (arguments.length === 2) {
-        // getFilter('March 1st', 'YYYYMMDD')
+        // getFilter('Dec 25, 1995', 'YYYYMMDD')
         var format = arguments[1];
 
         return moment(dateString).format(format);
     } else {
-        // getFilter('March 1st', 'MM-DD-YYYY', true)
+        // getFilter('Dec 25, 1995', 'MM-DD-YYYY', true)
         var format = arguments[1];
 
         return moment(dateString).format(format).replace(meridiemRegEx, "$1.m.");
@@ -30,6 +31,13 @@ module.exports = getFilter;
  */
 module.exports.setDefaultFormat = function(format) {
     return defaultFormat = format;
+};
+
+/**
+ * Set user-specified default format for date
+ */
+module.exports.setCustomName = function(name) {
+    return customName = name;
 };
 
 
