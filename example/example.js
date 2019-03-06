3c1b2b63303b05cefcbe332e1ac26af8cbb064bd
Sophie Idromenos <techmsi@users.noreply.github.com>
2019-03-06 16:04:28 -0500

    Update example.js

--- example/example.js
+++ example/example.js
@@ -1,7 +1,9 @@
 // Import nunjucks
 var nunjucks = require('nunjucks');
+
 // Import the plugin
 var nunjucksDate = require('nunjucks-date');
+var moment = require('moment');
 
 // Define a custom default date format. Any valid format works.
 // The date format defaults to "YYYY"
@@ -16,3 +18,14 @@ nunjucksDate.install(env, 'mySpecialDateFilter');
 
 // Print out environment filters to ensure filter is present
 console.log('Now your filter should be listed here: ', env.filters);
+
+// Example Usage
+var dateStringTest = '1995-12-25';
+var currentDate = env.renderString(` 
+  Present: {{ present | mySpecialDateFilter}} & Past: {{ past }}
+`, { 
+  present: moment(), 
+  past: moment(dateStringTest).fromNow() 
+});
+
+console.log('currentDate', currentDate);
